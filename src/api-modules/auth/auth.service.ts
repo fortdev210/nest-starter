4f21321740050887ceddef6d1d9aa343c11f4a3d
fortdev210 <sharp.blader2050@gmail.com>
2023-03-17 17:54:27 +0800

    add user module

--- src/api-modules/auth/auth.service.ts
+++ src/api-modules/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from "bcrypt";
+
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    // private userService: UserService
+    private configService: ConfigService,
+  ) {}
+
+  private hashPassword(password: string): string {
+    return bcrypt.hashSync(password, this.configService.get('bcrypt.salt'))
+  }
+}
