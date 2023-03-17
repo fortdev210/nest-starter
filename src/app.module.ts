6442cf920666919dadcf80e0c4d24e07cb5fe22e
fortdev210 <sharp.blader2050@gmail.com>
2023-03-17 17:32:19 +0800

    add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { PlansModule } from './api-modules/plans/plans.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, PlansModule],
+  imports: [
+    PrismaModule,
+    PlansModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
