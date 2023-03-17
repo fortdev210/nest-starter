497970a9e6a55a675d1c511b6b20888511b8f54e
fortdev210 <sharp.blader2050@gmail.com>
2023-03-17 18:02:04 +0800

    add create user service

--- src/api-modules/users/dto/create-user.dto.ts
+++ src/api-modules/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  @Transform(({ value }) => `${value}`.toLowerCase())
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  last_name: string;
+}
